5bd445ae28a0f0c691951dec7ca12db385c3c8bb
JaymeeDEV <jamiekellybrown@gmail.com>
2019-08-01 15:11:09 +0100

    Create seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque tortor vitae tempus scelerisque. Sed vitae eros condimentum ligula venenatis placerat ut in mauris. Etiam orci sapien, volutpat eu volutpat eget, ultricies eget ipsum. Praesent nec laoreet lacus, id aliquet dolor. Nullam vel diam felis. Nam rutrum neque et metus congue, quis auctor quam commodo. Nullam dignissim quam metus, vel sollicitudin massa sodales sed. Vivamus eget odio eget quam hendrerit iaculis eget sed ex. Fusce massa turpis, mattis vel lacinia id, tempor sed nibh. Proin sit amet nulla vel leo lobortis faucibus. Suspendisse potenti. Mauris sed purus dolor. Aliquam erat volutpat. Donec varius placerat tortor, a varius sapien aliquam eu."
+  )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "My List of Skills #{skill}",
+    percent_utilized: 15
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}" ,
+    subtitle: "My great service" ,
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque tortor vitae tempus scelerisque. Sed vitae eros condimentum ligula venenatis placerat ut in mauris. Etiam orci sapien, volutpat eu volutpat eget, ultricies eget ipsum. Praesent nec laoreet lacus, id aliquet dolor. Nullam vel diam felis. Nam rutrum neque et metus congue, quis auctor quam commodo. Nullam dignissim quam metus, vel sollicitudin massa sodales sed. Vivamus eget odio eget quam hendrerit iaculis eget sed ex. Fusce massa turpis, mattis vel lacinia id, tempor sed nibh. Proin sit amet nulla vel leo lobortis faucibus. Suspendisse potenti. Mauris sed purus dolor. Aliquam erat volutpat. Donec varius placerat tortor, a varius sapien aliquam eu.",
+    main_image: "https://via.placeholder.com/350",
+    thumb_image:"https://via.placeholder.com/150"
+  )
+end
+
+puts "9 portfolio items created"
